3b68ee94613dffdf88f9120717688defaa405d29
shossen1999 <shossen1999@gmail.com>
2023-09-29 12:33:56 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+	include "dbconnect.php";
+	
+	$s="SELECT * FROM student";
+	$result=$conn->query($s);
+	
+	//echo "<h1>adhhd</h1>";
+	/*
+	while($r = $result->fetch_assoc())
+	{
+		$name=$r['name'];
+		echo $name;
+		echo "ajgdsdjk";
+	}
+	*/
+	//echo $r['email'];
+	//echo gettype($r);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<style>
+		table, td, th{
+			border-collapse:collapse;
+			border:2px solid black;
+		}
+		table{
+			width:80%;
+			margin: 0 auto;
+		}
+		td, th{
+			padding:15px;
+			text-align:center;
+		}
+		
+	</style>
+</head>
+<body>	
+	
+		<center>
+			<h1> Student List</h1>
+			<table>
+				<tr>
+					<th>ID</th>
+					<th>Name</th>
+					<th>Roll</th>
+					<th>Email</th>
+					<th>EDIT</th>
+					<th>Delete</th>
+				</tr>
+				<?php
+				//$i=1;
+				while($r = $result->fetch_assoc())
+				{
+					$idd=$r['id'];
+					$nam=$r['name'];
+					$rol=$r['roll'];
+					$eml=$r['email'];
+					echo "<tr>";
+						echo "<td>". $idd . "</td>";
+						echo "<td>". $nam . "</td>";
+						echo "<td>". $rol . "</td>";
+						echo "<td>". $eml . "</td>";
+						echo "<td> <a href='editForm.php?edit_id=$idd'>Edit</a></td>";
+						echo "<td> <a href='delete.php?del_id=$idd'>Delete</a></td>";
+					echo "</tr>";
+				}
+				?>
+				
+				<tr>
+					<td colspan="5"><a href="insertForm.php">Insert Record</a></td>
+				</tr>
+			</table>
+		</center>
+	
+</body>
+</html>
+
+
+
+
